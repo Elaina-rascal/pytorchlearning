c93d72bbade224612d3875bf39c6c6443d3f48c7
Elaina <1463967532@qq.com>
2025-09-28 23:15:39 +0800

    不采用外部仓库

--- .devcontainer/cpu/build.bash
+++ .devcontainer/cpu/build.bash
@@ -12,7 +12,7 @@ echo "父目录: $PARENT_DIR"
 TAG="pytorch"
 
 # 从外部传入的 IMAGE_REPO（格式：ghcr.io/user/repo 或 docker.io/user/repo）
-IMAGE_REPO=${IMAGE_REPO:-elainasuki/other}
+IMAGE_REPO="elainasuki/other"
 
 # 组合完整镜像名
 IMAGE="$IMAGE_REPO:$TAG"
